680130333564654cf45b8d9dd89c7fa3ce173fae
QuimDJ <quimdalmases@uoc.edu>
2023-06-15 10:50:20 +0200

    upd
    .

---
+++ Leccion6/11.2.1_dynamic_array.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.1;
+contract Example {
+    uint[][] public ListOfLists;
+
+    function getArray(uint index) public view returns(uint[] memory)
+    {
+        return ListOfLists[index];
+    }
+
+    function addElement2Array(uint index, uint value) public {
+        ListOfLists[index].push(value);
+    }
+    function totalLists() public view returns(uint) {
+        return ListOfLists.length;
+    }
+    function addList(uint[] memory a) public {
+        ListOfLists.push(a);
+    }
+}
